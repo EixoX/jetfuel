13746d1073785aaa5a72c9a9e2c74bae487ad3ed
Giovanna <marinelli.giovanna@gmail.com>
2019-01-23 16:06:54 -0200

    Update MsSqlServerCommand.java

--- jetfuel-core/src/main/java/com/eixox/data/sql/MsSqlServerCommand.java
+++ jetfuel-core/src/main/java/com/eixox/data/sql/MsSqlServerCommand.java
@@ -13,4 +13,19 @@ public class MsSqlServerCommand extends DatabaseCommand {
 				: appendNumber(0);
 	}
 
+	@Override
+	public DatabaseCommand appendByteArray(byte[] value) {
+		char[] hexArray = "0123456789ABCDEF".toCharArray();
+		super.appendRaw("0x");
+		char[] hexChars = new char[value.length * 2];
+		for (int j = 0; j < value.length; j++) {
+			int v = value[j] & 0xFF;
+			hexChars[j * 2] = hexArray[v >>> 4];
+			hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+		}
+		super.text.append(hexChars);
+		return this;
+
+	}
+
 }
